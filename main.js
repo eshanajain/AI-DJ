7a127c4886bc772c1c3e6c4189eaff69ed936843
eshanajain <82397896+eshanajain@users.noreply.github.com>
2022-02-11 16:37:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,8 +24,10 @@ function gotPoses(results)
 {
     if(results.length>0){
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
        scoreLeftwrist = results[0].pose.keypoints[9].score;
        console.log("scoreleftWrist = " + scoreLeftwrist);
+       console.log("scoreRightWrist = " + scoreRightWrist );
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
@@ -42,6 +44,42 @@ function draw(){
 
     fill("#f0f8ff");
     stroke("#faebd7");
+
+if(scoreRightWrist > 0.2)
+{
+    circle(rightWristX,rightWristY,20);
+
+    if(rightWristY >0 && rightWristY<= 100)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 0.5x";
+        song.rate(0.5);
+    }
+
+    else if(rightWristY >100 && rightWristY<= 200)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1x";
+        song.rate(1);
+    }
+
+    else if(rightWristY >200 && rightWristY<= 300)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1.5x";
+        song.rate(1.5);
+    }
+   
+    else if(rightWristY >300 && rightWristY<= 400)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 2x";
+        song.rate(2);
+    }
+    else if(rightWristY >400 && rightWristY<= 500)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 2.5x";
+        song.rate(2.5);
+    }
+
+
+}
 
     if(scoreLeftWrist > 0.2)
     {
